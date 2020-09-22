db6486e6f3284fe142040264a8bd970584d02d57
Leroy Hopson <git@leroy.geek.nz>
2020-09-22 15:31:46 +0700

    Add asciicast importer
    Former-commit-id: ffa85618659024515cdde252c56c7be34a245446

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 ### Added
 - Changelog.
+- Asciicast importer plugin. Enables the import of .cast ([asciicast files v2](https://github.com/asciinema/asciinema/blob/master/doc/asciicast-v2.md)) that can be made using the [asciinema](https://asciinema.org/) terminal session recorder. See the [asciicast scene](/examples/asciicast) for example usage.
 
 ### Changed
 - Implementation of Terminal node from GDScript to GDNative using [Aetf's patched version of libtsm](https://github.com/Aetf/libtsm).
--- addons/godot_xterm/plugin.gd
+++ addons/godot_xterm/plugin.gd
@@ -2,7 +2,16 @@ tool
 extends EditorPlugin
 
 
+var asciicast_import_plugin
+
+
 func _enter_tree():
+	asciicast_import_plugin = preload("res://addons/godot_xterm/import_plugins/asciicast_import_plugin.gd").new()
+	add_import_plugin(asciicast_import_plugin)
+	
+	var asciicast_script = preload("res://addons/godot_xterm/resources/asciicast.gd")
+	add_custom_type("Asciicast", "Animation", asciicast_script, null)
+	
 	var terminal_script = preload("res://addons/godot_xterm/nodes/terminal/terminal.gdns")
 	var terminal_icon = preload("res://addons/godot_xterm/nodes/terminal/terminal_icon.svg")
 	add_custom_type("Terminal", "Control", terminal_script, terminal_icon)
@@ -13,5 +22,9 @@ func _enter_tree():
 
 
 func _exit_tree():
+	remove_import_plugin(asciicast_import_plugin)
+	asciicast_import_plugin = null
+	
+	remove_custom_type("Asciicast")
 	remove_custom_type("Terminal")
 	remove_custom_type("Psuedoterminal")
