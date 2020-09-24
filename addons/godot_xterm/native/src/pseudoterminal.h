9d06d7c31382e901f0bcec457d4b438349e287eb
Leroy Hopson <git@leroy.geek.nz>
2020-09-24 14:27:26 +0700

    Update file format
    Automatically updated by VSCode upon saving the files.
    
    
    Former-commit-id: 0dabc5607640f385da4fa5eb73a6bbf6e104371d

--- addons/godot_xterm/native/src/pseudoterminal.h
+++ addons/godot_xterm/native/src/pseudoterminal.h
@@ -9,39 +9,39 @@
 namespace godot
 {
 
-class Pseudoterminal : public Node
-{
-    GODOT_CLASS(Pseudoterminal, Node)
+    class Pseudoterminal : public Node
+    {
+        GODOT_CLASS(Pseudoterminal, Node)
 
-public:
-    static const int MAX_READ_BUFFER_LENGTH = 1024;
-    static const int MAX_WRITE_BUFFER_LENGTH = 1024;
+    public:
+        static const int MAX_READ_BUFFER_LENGTH = 1024;
+        static const int MAX_WRITE_BUFFER_LENGTH = 1024;
 
-private:
-    std::thread pty_thread;
-    bool should_process_pty;
+    private:
+        std::thread pty_thread;
+        bool should_process_pty;
 
-    char write_buffer[MAX_WRITE_BUFFER_LENGTH];
-    int bytes_to_write;
-    std::mutex write_buffer_mutex;
+        char write_buffer[MAX_WRITE_BUFFER_LENGTH];
+        int bytes_to_write;
+        std::mutex write_buffer_mutex;
 
-    char read_buffer[MAX_READ_BUFFER_LENGTH];
-    int bytes_to_read;
-    std::mutex read_buffer_mutex;
+        char read_buffer[MAX_READ_BUFFER_LENGTH];
+        int bytes_to_read;
+        std::mutex read_buffer_mutex;
 
-    void process_pty();
+        void process_pty();
 
-public:
-    static void _register_methods();
+    public:
+        static void _register_methods();
 
-    Pseudoterminal();
-    ~Pseudoterminal();
+        Pseudoterminal();
+        ~Pseudoterminal();
 
-    void _init();
-    void _ready();
+        void _init();
+        void _ready();
 
-    void put_data(PoolByteArray data);
-};
+        void put_data(PoolByteArray data);
+    };
 } // namespace godot
 
 #endif // PSEUDOTERMINAL_H
