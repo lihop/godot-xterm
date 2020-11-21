6689ad1c096a8e31cae294c9e73e745f63b96322
Leroy Hopson <git@leroy.geek.nz>
2020-11-21 19:00:40 +0700

    Remove cross-compilation dependencies
    It makes the github actions workflow take longer and cross-compiled
    artifacts are not currently used (nor do they seem to work).

--- addons/godot_xterm/native/shell.nix
+++ addons/godot_xterm/native/shell.nix
@@ -1,22 +1,9 @@
-# Use --argstr system i686-linux to build for 32-bit linux.
-{ system ? builtins.currentSystem }:
-with (import <nixpkgs> {
-  inherit system;
-});
+with (import <nixpkgs> {});
 mkShell {
   buildInputs = with pkgs; [
-    binutils.bintools
-    cacert
-    cmake
     git
-    libxkbcommon
-    nix
-    pkg-config
     scons
-  ] ++ lib.optionals (system == builtins.currentSystem) [
-    # Additional dependencies for cross-compiling for Windows and OSX.
-    clang
-    pkgsCross.mingwW64.buildPackages.gcc
-    pkgsCross.mingw32.buildPackages.gcc
+
+    cacert # Required for git clone on GithHub actions runner.
   ];
 }
