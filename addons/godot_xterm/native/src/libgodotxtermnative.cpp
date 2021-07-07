44ea56aa0f1176dd4d027f5612b171207c5f3611
Leroy Hopson <git@leroy.geek.nz>
2021-07-07 21:26:48 +0700

    Add disable_pty compile option
    If disable_pty=yes then the PTY node and dependencies (LibuvUtils, Pipe)
    will be excluded. On platforms where the PTY node is not supported (e.g.
    HTML5), this will always be equivalent to `disable_pty=yes`.

--- addons/godot_xterm/native/src/libgodotxtermnative.cpp
+++ addons/godot_xterm/native/src/libgodotxtermnative.cpp
@@ -18,9 +18,11 @@ godot_gdnative_terminate(godot_gdnative_terminate_options *o) {
 extern "C" void GDN_EXPORT godot_nativescript_init(void *handle) {
   godot::Godot::nativescript_init(handle);
   godot::register_tool_class<godot::Terminal>();
-#if !defined(__EMSCRIPTEN__) && !defined(__WIN32)
+#if !defined(_PTY_DISABLED)
   godot::register_tool_class<godot::Pipe>();
   godot::register_tool_class<godot::LibuvUtils>();
+#if defined(__unix__)
   godot::register_tool_class<godot::PTYUnix>();
+#endif
 #endif
 }
