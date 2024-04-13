ee733ca45a38decd55dc30eef865136fe7fbbf3a
Leroy Hopson <git@leroy.geek.nz>
2024-04-13 19:10:44 +1200

    chore(deps): add godot editor debugger plugin

--- plug.gd
+++ plug.gd
@@ -6,3 +6,6 @@ extends "res://addons/gd-plug/plug.gd"
 func _plugging():
 	plug("bitwes/Gut", {tag = "v9.2.0"})
 	plug("lihop/godot-pixelmatch", {tag = "v2.0.0", include = ["addons/pixelmatch"]})
+	plug(
+		"Zylann/godot_editor_debugger_plugin", {commit = "b0301fb58790c33ff1d839c4debddd4137ff90e9"}
+	)
